8cd4e44386740fcc4ee44895f58bd44cca548aed
Halit Cetin <halitcetin@live.com>
2021-04-25 23:01:47 +0300

    Initial commit

---
+++ netconfClient/MainWindow.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+using Renci.SshNet;
+
+namespace netconfClient
+{
+    /// <summary>
+    ///     MainWindow penceresi ile ilgili tüm lojik işlemler.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Private Variables
+        private string path { get; set; }
+        private bool justGotOnline { get; set; }
+        private bool resetButtonClicked { get; set; }
+
+        /// <summary>
+        ///     MainWindow penceresi için constructor fonksiyonu.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            path = @"C:\Ulak";
+            justGotOnline = false;
+            resetButtonClicked = false;
+            CheckXmlDirectory(path);
+            WriteClientXml(path, "online", "", "", "", "");
+            Task.Run(() => { CheckServerXml(); });
+        }
+
+        private async void CheckServerXml()
+        {
+            while(true)
+            {
+                await Task.Delay(100);
+
+                try
+                {
+                    await serverStatusTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        serverStatusTextBox.Text = DetectServerStatus();
+                    }));
+
+                    if (DetectServerStatus() == "online" && (justGotOnline == false || resetButtonClicked == true))
+                    {
+                        justGotOnline = true;
+
+                        if(resetButtonClicked == false)
+                        {
+                            await mainGrid.Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                mainGrid.IsEnabled = true;
+                            }));
+                        }
+                        else
+                        {
+                            resetButtonClicked = false;
+                        }
+
+                        await manNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            manNameTextBox.Text = ReadServerXml("manName");
+                        }));
+                        await uniqueIdTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            uniqueIdTextBox.Text = ReadServerXml("manUid");
+                        }));
+                        await modelNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            modelNameTextBox.Text = ReadServerXml("modelName");
+                        }));
+                        await serialTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            serialTextBox.Text = ReadServerXml("serialNo");
+                        }));
+                    }
+                    else if (DetectServerStatus() == "offline")
+                    {
+                        justGotOnline = false;
+
+                        await mainGrid.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            mainGrid.IsEnabled = false;
+                        }));
+
+                        await manNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            manNameTextBox.Text = "";
+                        }));
+                        await uniqueIdTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            uniqueIdTextBox.Text = "";
+                        }));
+                        await modelNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            modelNameTextBox.Text = "";
+                        }));
+                        await serialTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            serialTextBox.Text = "";
+                        }));
+
+                        WriteClientXml(path, "offline", "", "", "", "");
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Client XML Read Error !");
+                }
+            }
+        }
+
+        private string ReadServerXml(string searchTerm)
+        {
+            try
+            {
+                XDocument xmlFile = XDocument.Load(path + @"\serverConf.xml");
+                var query = xmlFile.Elements("netconf").Elements(searchTerm).ToArray();
+                return query[0].Value.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private string DetectServerStatus()
+        {
+            try
+            {
+                XDocument xmlFile = XDocument.Load(path + @"\server.xml");
+                var query = xmlFile.Elements("netconf").Elements("serverStatus").ToArray();
+                return query[0].Value.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        ///     Uygulama kapanmadan önce son kontrolleri yapar.
+        /// </summary>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            WriteClientXml(path, "offline", "", "", "", "");
+        }
+
+        /// <summary>
+        ///     XML haberleşmesi için gerekli klasörü oluşturur.
+        /// </summary>
+        private void CheckXmlDirectory(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path) == false)
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Directory Error!");
+            }
+        }
+
+        /// <summary>
+        ///     XML haberleşmesi için gerekli istemci dosyalarını oluşturur.
+        /// </summary>
+        private void WriteClientXml(string path, string status, string manName, string uniqueId, string modelName, string serialNumber)
+        {
+            try
+            {
+                XDocument doc = new XDocument(new XElement("netconf",
+                                                new XElement("clientStatus", status), new XElement("clientRequest",
+                                                                                new XElement("manName", manName), new XElement("uniqueId", uniqueId), new XElement("modelName", modelName), new XElement("serialNumber", serialNumber))));
+                doc.Save(path + @"\client.xml");
+            }
+            catch
+            {
+                MessageBox.Show("XML File Error!");
+            }
+        }
+
+        private void resetButton_Click(object sender, RoutedEventArgs e)
+        {
+            resetButtonClicked = true;
+            mainGrid.IsEnabled = false;
+            Task.Run(() => { EnableMainGrid(); });
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            WriteClientXml(path, "online", manNameTextBox.Text, uniqueIdTextBox.Text, modelNameTextBox.Text, serialTextBox.Text);
+            mainGrid.IsEnabled = false;
+            Task.Run(() => { EnableMainGrid(); });
+        }
+
+        private async void EnableMainGrid()
+        {
+            await Task.Delay(200);
+
+            await mainGrid.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                mainGrid.IsEnabled = true;
+            }));
+        }
+    }
+}
---
+++ nrtconfServer/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace nrtconfServer
+{
+    /// <summary>
+    ///     MainWindow penceresi ile ilgili tüm lojik işlemler.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Private Variables
+        private string path { get; set; }
+
+        /// <summary>
+        ///     MainWindow penceresi için constructor fonksiyonu.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            path = @"C:\Ulak";
+            CheckXmlDirectory(path);
+            WriteServerXml(path, "online");
+            Task.Run(() => { CheckClientXml(); });
+        }
+
+        /// <summary>
+        ///     Uygulama kapanmadan önce son kontrolleri yapar.
+        /// </summary>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            WriteServerXml(path, "offline");
+        }
+
+        /// <summary>
+        ///     XML haberleşmesi için gerekli klasörü oluşturur.
+        /// </summary>
+        private void CheckXmlDirectory(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path) == false)
+                {
+                    Directory.CreateDirectory(path);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Directory Create Error!");
+            }
+        }
+
+        /// <summary>
+        ///     XML haberleşmesi için gerekli istemci dosyalarını oluşturur.
+        /// </summary>
+        private void WriteServerXml(string path, string status)
+        {
+            try
+            {
+                XDocument doc = new XDocument(new XElement("netconf",
+                                                new XElement("serverStatus", status)));
+
+                doc.Save(path + @"\server.xml");
+
+                doc = new XDocument(new XElement("netconf",
+                                        new XElement("manName", "Netas"), new XElement("manUid", "NNTM31"), new XElement("modelName", "ULAKMacroENB"), new XElement("serialNo", "0K7558")));
+                doc.Save(path + @"\serverConf.xml");
+            }
+            catch
+            {
+                MessageBox.Show("Config XML File Create Error!");
+            }
+        }
+
+        /// <summary>
+        ///     Netconf client isteklerini handle eder.
+        /// </summary>
+        private async void CheckClientXml()
+        {
+            while(true)
+            {
+                await Task.Delay(100);
+
+                try
+                {
+                    await clientStatusTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        clientStatusTextBox.Text = DetectClientStatus();
+                    }));
+
+                    if (DetectClientStatus() == "online")
+                    {
+                        DetectClientRequest();
+                    }
+                    ReadXmlConfigAndWriteTextBoxes();
+                }
+                catch 
+                {
+                    MessageBox.Show("Client XML Read Error !");
+                }
+            }
+        }
+
+        private string DetectClientStatus()
+        {
+            try
+            {
+                XDocument xmlFile = XDocument.Load(path + @"\client.xml");
+                var query = xmlFile.Elements("netconf").Elements("clientStatus").ToArray();
+                return query[0].Value.ToString();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void DetectClientRequest()
+        {
+            string manName;
+            string uniqueId;
+            string modelName;
+            string serialNumber;
+
+            XDocument xmlFile = XDocument.Load(path + @"\client.xml");
+
+            var query = xmlFile.Elements("netconf").Elements("clientRequest").Elements("manName").ToArray();
+
+            if (query[0].Value.ToString() != null)
+            {
+                manName = query[0].Value.ToString();
+            }
+            else
+            {
+                manName = "";
+            }
+
+            query = xmlFile.Elements("netconf").Elements("clientRequest").Elements("uniqueId").ToArray();
+            if (query[0].Value.ToString() != null)
+            {
+                uniqueId = query[0].Value.ToString();
+            }
+            else
+            {
+                uniqueId = "";
+            }
+
+            query = xmlFile.Elements("netconf").Elements("clientRequest").Elements("modelName").ToArray();
+            if (query[0].Value.ToString() != null)
+            {
+                modelName = query[0].Value.ToString();
+            }
+            else
+            {
+                modelName = "";
+            }
+
+            query = xmlFile.Elements("netconf").Elements("clientRequest").Elements("serialNumber").ToArray();
+            if (query[0].Value.ToString() != null)
+            {
+                serialNumber = query[0].Value.ToString();
+            }
+            else
+            {
+                serialNumber = "";
+            }
+
+            if (manName != "" && uniqueId != "" && modelName != "" && serialNumber != "")
+            {
+                try
+                {
+                    XDocument doc = new XDocument(new XElement("netconf",
+                                            new XElement("manName", manName), new XElement("manUid", uniqueId), new XElement("modelName", modelName), new XElement("serialNo", serialNumber)));
+                    doc.Save(path + @"\serverConf.xml");
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async void ReadXmlConfigAndWriteTextBoxes()
+        {
+            try
+            {
+                XDocument xmlFile = XDocument.Load(path + @"\serverConf.xml");
+                
+                var query = xmlFile.Elements("netconf").Elements("manName").ToArray();
+                await manNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    manNameTextBox.Text = query[0].Value.ToString();
+                }));
+
+                query = xmlFile.Elements("netconf").Elements("manUid").ToArray();
+                await uniqueIdTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    uniqueIdTextBox.Text = query[0].Value.ToString();
+                }));
+
+                query = xmlFile.Elements("netconf").Elements("modelName").ToArray();
+                await modelNameTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    modelNameTextBox.Text = query[0].Value.ToString();
+                }));
+
+                query = xmlFile.Elements("netconf").Elements("serialNo").ToArray();
+                await serialTextBox.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    serialTextBox.Text = query[0].Value.ToString();
+                }));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
